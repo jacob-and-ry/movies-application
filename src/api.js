65ddbbb2ae80c4bd6cef8b9f20c4a6b4eb9d75a7
Ry Sutton <ryancesutton@gmail.com>
2020-04-28 14:55:40 -0500

    working on add movie functionality

--- src/api.js
+++ src/api.js
@@ -2,5 +2,16 @@ module.exports = {
   getMovies: () => {
     return fetch('/api/movies')
       .then(response => response.json());
+  },
+
+  postMovie: (movieData) => {
+    return fetch('/api/movies', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(movieData),
+    }).then( response => response.json() )
+        .then( data => console.log(data) )
   }
 };
